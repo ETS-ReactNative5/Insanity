1c01757b62cd964309ce43faf0e093a659f81d16
Toan <zkai999@gmail.com>
2022-03-16 14:21:55 +0700

    Ios

--- src/modules/components/ComponentsView.js
+++ src/modules/components/ComponentsView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, View, Text, ScrollView } from 'react-native';
+import { StyleSheet, View, Text,  } from 'react-native';
 import { useRoute } from '@react-navigation/native';
 
 import Icon from 'react-native-vector-icons/Entypo';
